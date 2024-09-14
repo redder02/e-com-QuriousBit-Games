1c76187749ddad7ae50fc17ced8f8ba7dfe5cbff
redder02 <pranjal.2022ca065@mnnit.ac.in>
2024-09-14 17:21:32 +0530

    making the current code descriptive

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+# this class would be having all the product details
 class Product:
     def __init__(self, product_id, name, price, category):
         self.product_id = product_id
@@ -6,36 +8,48 @@ class Product:
         self.category = category
 
 
+# this class will be responsible for individual items of the class
 class CartItem:
     def __init__(self, product, quantity):
         self.product = product
         self.quantity = quantity
 
+    #calculating total price based on quantity and price
     def total_price(self):
         return self.product.price * self.quantity
 
 
+# current situation of the cart
 class Cart:
     def __init__(self):
         self.items = []
 
+    # adding item into the cart
     def add_item(self, product, quantity):
+
+        # checking if item is already present in the cart
         for item in self.items:
             if item.product.product_id == product.product_id:
                 item.quantity += quantity
                 return
+        # if item is not present in cart then add to the list
         self.items.append(CartItem(product, quantity))
 
+    # removing items from the cart
     def remove_item(self, product_id, quantity=None):
+
+        
         for item in self.items:
+
+            # if product quantity not given by customer then remove all the products 
             if item.product.product_id == product_id:
                 if quantity is None or quantity >= item.quantity:
                     self.items.remove(item)
                 else:
-                    item.quantity -= quantity
+                    item.quantity -= quantity # if quantity specified then reduce that much quantity only
                 return
 
-
+    # current cart snapshot
     def view_cart(self):
         total_cost = 0
         print("Your Cart:")
